fdb39f1754a5abd7cc45f9def4edaf5880f14766
Alexandre Da Silva <dev.dasilva.alexandre@gmail.com>
2018-03-09 19:48:37 +0100

    SchemaBuilder OK.

---
+++ DgraphNet.Client.Extensions.Tests/SchemaBuilderTest.cs
@@ -0,0 +1,60 @@
+﻿using DgraphNet.Client.Extensions.Builders;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DgraphNet.Client.Extensions.Tests
+{
+    [TestFixture]
+    public class SchemaBuilderTest
+    {
+        [Test]
+        public void test_build_string_predicate()
+        {
+            var schema = Schema.Predicate("first")
+                .String()
+                .Index(StringIndexType.Hash|StringIndexType.Term|StringIndexType.FullText)
+                .List()
+                .Count()
+                .Upsert()
+                .Build();
+
+            string exp = "first: [string] @index(hash, term, fulltext) @count @upsert .";
+
+            Assert.AreEqual(exp, schema);
+        }
+
+        [Test]
+        public void test_build_basic_predicate()
+        {
+            var schema = Schema.Predicate("first")
+                .Int()
+                .Index()
+                .List()
+                .Count()
+                .Upsert()
+                .Build();
+
+            string exp = "first: [int] @index @count @upsert .";
+
+            Assert.AreEqual(exp, schema);
+        }
+
+        [Test]
+        public void test_build_datetime_predicate()
+        {
+            var schema = Schema.Predicate("first")
+                .DateTime()
+                .Index(DateTimeIndexType.Month)
+                .List()
+                .Count()
+                .Upsert()
+                .Build();
+
+            string exp = "first: [datetime] @index(month) @count @upsert .";
+
+            Assert.AreEqual(exp, schema);
+        }
+    }
+}
---
+++ DgraphNet.Client.Extensions/Builders/NQuadBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DgraphNet.Client.Extensions.Builders
+{
+    public class NQuadBuilder
+    {
+    }
+}
